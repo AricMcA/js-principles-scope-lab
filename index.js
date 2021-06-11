a656f8469a54414539a7517d3884bd76d61cb323
Aric McAlister <aricmca@gmail.com>
2021-06-11 11:03:32 -0500

    commit tree f0105b0719f0075688962df33cb8abef83b01fbc

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    'BOB'.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'tyler';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'jackson'
+}
